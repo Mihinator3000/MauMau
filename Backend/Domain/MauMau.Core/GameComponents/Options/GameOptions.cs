01fd5f0d16a56ef890d7d31d2b6607ece574719e
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2023-01-28 01:43:54 +0300

    feat: game entities

---
+++ Backend/Domain/MauMau.Core/GameComponents/Options/GameOptions.cs
@@ -0,0 +1,58 @@
+﻿using MauMau.Common.Enums.Cards;
+using MauMau.Common.Exceptions;
+using RichEntity.Annotations;
+
+namespace MauMau.Core.GameComponents.Options;
+
+public partial class GameOptions : IEntity<Guid>
+{
+    private readonly List<PlayerPosition> _playerPositions;
+
+    public GameOptions(
+        long stake,
+        int playerCount,
+        Rank minCardRank,
+        int handSize,
+        string? password = null)
+        : this(Guid.NewGuid())
+    {
+        Stake = stake;
+        PlayerCount = playerCount;
+        MinCardRank = minCardRank;
+        HandSize = handSize;
+        Password = password;
+
+        _playerPositions = new List<PlayerPosition>();
+    }
+
+    public long Stake { get; protected init; }
+
+    public int PlayerCount { get; protected init; }
+    public Rank MinCardRank { get; protected init; }
+    public int HandSize { get; protected init; }
+
+    public string? Password { get; protected init; }
+
+    public bool IsOpen => string.IsNullOrEmpty(Password);
+
+    public IReadOnlyCollection<PlayerPosition> PlayerPositions => _playerPositions;
+
+    public bool IsFull => PlayerCount == _playerPositions.Count;
+
+    public void AddPlayerPosition(PlayerPosition playerPosition)
+    {
+        if (playerPosition.Position < 0 || playerPosition.Position > PlayerCount)
+            throw new DomainInvalidOperationException($"Position of player {playerPosition.Player} is incorrect");
+
+        _playerPositions.Add(playerPosition);
+    }
+
+    public void RemovePlayerPosition(PlayerPosition playerPosition)
+    {
+        if (!_playerPositions.Remove(playerPosition))
+            throw new DomainInvalidOperationException($"Position of player {playerPosition.Player} could not be removed");
+    }
+
+    public override string ToString()
+        => $"Id: {Id}";
+}
