a6b9e9c8f94f0a2fcc54201780872d92042bd8b2
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2023-02-01 03:03:29 +0300

    feat: moves

---
+++ Backend/Infrastructure/GameLogic/MauMau.GameLogic/Models/Moves/CardMove.cs
@@ -0,0 +1,59 @@
+﻿using MauMau.Abstractions.GameLogic.Cards;
+using MauMau.Abstractions.GameLogic.Models;
+using MauMau.Common.Exceptions;
+using MauMau.GameLogic.Cards.Abstractions;
+using MauMau.GameLogic.Cards.Actions;
+using MauMau.GameLogic.Cards.NonDraw.Helpers;
+
+namespace MauMau.GameLogic.Models.Moves;
+
+public record CardMove(Guid HandId, ICard Card) : Move(HandId)
+{
+    public override bool CanBePlayed(IReadOnlyList<IMove> moves)
+    {
+        for (int i = 0; i < moves.Count; i++)
+        {
+            switch (moves[^i])
+            {
+                case CardMove cardMove:
+                    return Card.CanBePlayed(cardMove.Card);
+                case PickSuitMove pickSuitMove:
+                    var helperCard = new HelperCard(pickSuitMove.Suit);
+                    return Card.CanBePlayed(helperCard);
+            }
+        }
+
+        throw new GameLogicException("Game should have card or pick moves");
+    }
+
+    public override void Play(IGame game)
+    {
+        game.CurrentHand.PlayCardToPile(Card, game.Pile);
+
+        if (Card is not IActionCard actionCard)
+        {
+            game.SetNextHandCurrent();
+            return;
+        }
+
+        switch (actionCard.GetCardAction())
+        {
+            case NotChangeHandAction:
+                return;
+            case SkipAction:
+                game.SetNextHandCurrent();
+                game.PlayMove(new SkipMove(game.CurrentHand.Id));
+                return;
+            case ChangeRotationAction:
+                game.ChangeRotation();
+                game.SetNextHandCurrent();
+                return;
+            case DrawWithoutResponseAction:
+                game.SetNextHandCurrent();
+                game.PlayMove(new DrawMove(game.CurrentHand.Id));
+                return;
+            default:
+                throw new GameLogicException("Action is not supported");
+        }
+    }
+}
---
+++ Backend/Infrastructure/GameLogic/MauMau.GameLogic/Models/Moves/SkipMove.cs
@@ -0,0 +1,20 @@
+﻿using MauMau.Abstractions.GameLogic.Models;
+using MauMau.GameLogic.Cards.NonDraw;
+
+namespace MauMau.GameLogic.Models.Moves;
+
+public record SkipMove(Guid HandId) : Move(HandId)
+{
+    public override bool CanBePlayed(IReadOnlyList<IMove> moves)
+    {
+        return moves[^1] switch
+        {
+            CardMove {Card: AceCard} => true,
+            DrawMove => !EightInThreeMovesWithOnlyDraws(moves),
+            _ => false
+        };
+    }
+
+    public override void Play(IGame game)
+        => game.SetNextHandCurrent();
+}
