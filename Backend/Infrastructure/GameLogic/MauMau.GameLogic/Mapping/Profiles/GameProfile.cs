46005f1c0b2fc28b45bb6f863a8a59e3fcd50307
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2023-02-01 03:05:13 +0300

    feat: mapping profiles

---
+++ Backend/Infrastructure/GameLogic/MauMau.GameLogic/Mapping/Profiles/GameProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MauMau.Abstractions.GameLogic.Models;
+using MauMau.Application.Dto.Game;
+using MauMau.GameLogic.Mapping.Converters;
+
+namespace MauMau.GameLogic.Mapping.Profiles;
+
+public class GameProfile : Profile
+{
+    public GameProfile()
+    {
+        CreateMap<IGame, GameDto>();
+
+        CreateMap<GameDto, IGame>()
+            .ConvertUsing<GameConverter>();
+    }
+}
