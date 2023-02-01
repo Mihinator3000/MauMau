ef061e855a20e6c8034b65b93e2c7751db203cb0
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2023-02-01 03:05:00 +0300

    feat: mapping converters

---
+++ Backend/Infrastructure/GameLogic/MauMau.GameLogic/Mapping/Converters/GameConverter.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using MauMau.Abstractions.GameLogic.Models;
+using MauMau.Abstractions.GameLogic.Providers;
+using MauMau.Application.Dto.Game;
+using MauMau.GameLogic.Models;
+
+namespace MauMau.GameLogic.Mapping.Converters;
+
+public class GameConverter : ITypeConverter<GameDto, IGame>
+{
+    private readonly IRandomProvider _randomProvider;
+
+    public GameConverter(IRandomProvider randomProvider)
+    {
+        _randomProvider = randomProvider;
+    }
+
+    public IGame Convert(GameDto dto, IGame destination, ResolutionContext context)
+    {
+        var mapper = context.Mapper;
+
+        var hands = mapper.Map<List<IHand>>(dto.Hands);
+        var deck = mapper.Map<IDeck>(dto.Deck);
+        var pile = mapper.Map<IPile>(dto.Pile);
+        var moves = mapper.Map<List<IMove>>(dto.Moves);
+
+        var random = _randomProvider.GetRandom();
+
+        return new Game(
+            dto.Id,
+            dto.WinMoney,
+            hands,
+            deck,
+            pile,
+            moves,
+            random,
+            dto.MovesLeftCount,
+            dto.CurrentHandNumber,
+            dto.IsRotationClockWise);
+    }
+}
