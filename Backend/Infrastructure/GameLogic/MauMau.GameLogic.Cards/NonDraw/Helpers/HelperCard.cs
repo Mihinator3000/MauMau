3a5b7b20943936d6e1735171d9f53b3ca0967c78
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2023-02-01 03:01:39 +0300

    feat: non draw cards

---
+++ Backend/Infrastructure/GameLogic/MauMau.GameLogic.Cards/NonDraw/Helpers/HelperCard.cs
@@ -0,0 +1,9 @@
+﻿using MauMau.Common.Enums.Cards;
+using MauMau.GameLogic.Cards.Abstractions;
+
+namespace MauMau.GameLogic.Cards.NonDraw.Helpers;
+
+public class HelperCard : NonDrawCard
+{
+    public HelperCard(Suit suit, Rank rank = Rank.Undefined) : base(rank, suit) { }
+}
