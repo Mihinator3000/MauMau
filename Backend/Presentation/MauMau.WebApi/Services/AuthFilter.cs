37781ce33ed3759add6e1cfe52b18c6ba8b10600
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2023-02-02 22:01:29 +0300

    feat: auth filter

---
+++ Backend/Presentation/MauMau.WebApi/Services/AuthFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MauMau.WebApi.Services;
+
+public class AuthFilter : IHubFilter
+{
+    public async ValueTask<object?> InvokeMethodAsync(
+        HubInvocationContext invocationContext,
+        Func<HubInvocationContext, ValueTask<object?>> next)
+    {
+        HttpContext? httpContext = invocationContext.Context.GetHttpContext();
+
+        string? authCookie = httpContext?.Request.Cookies["AuthCookie"];
+
+        if (string.IsNullOrEmpty(authCookie) || !IsCookieValid(authCookie))
+        {
+            //throw new HubException("Unauthorized");
+        }
+        
+        return await next(invocationContext);
+    }
+
+    private bool IsCookieValid(string authCookie)
+    {
+        throw new NotImplementedException();
+    }
+}
