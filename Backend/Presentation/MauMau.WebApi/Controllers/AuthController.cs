1bd22680cac2fb6a4a5eaa0ba5153e73976edb4f
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2023-02-02 22:01:48 +0300

    feat: auth controllers

---
+++ Backend/Presentation/MauMau.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using MauMau.Application.Dto.Identity;
+using MauMau.Application.Requests.Identity;
+using Mediator;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MauMau.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public AuthController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginDataDto loginData)
+    {
+        var request = new Login.Request(loginData);
+        Login.Response response = await _mediator.Send(request);
+        return Ok(response.AccessToken);
+    }
+}
